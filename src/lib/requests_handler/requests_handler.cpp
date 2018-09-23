04590ba77f6de929958e3a0037555081a71764fc
sharonronen <sharonronen@sharons-air.lan>
2018-09-23 15:54:02 +1000

    adding more comments to the code

--- src/lib/requests_handler/requests_handler.cpp
+++ src/lib/requests_handler/requests_handler.cpp
@@ -7,24 +7,29 @@ using namespace std;
 
 RequestsHadler::RequestsHadler(int requests_limit, int timer_rate_in_sec) :
 		requests_limit_(requests_limit),
-		current_requests_limit_(requests_limit){
+		current_requests_limit_(requests_limit)
+        timer_rate_in_sec_(timer_rate_in_sec){
 	assert(requests_limit > 0);
 	assert(timer_rate_in_sec > 0);
 }
 
 
 void RequestsHadler::requests_limit(int requests_limit){
+	//requests_limit can't be negative
 	assert(requests_limit > 0);
 	requests_limit_ = requests_limit;
 }
 
 void RequestsHadler::timer_rate_in_sec(int timer_rate_in_sec){
+	//timer_rate_in_sec can't be negative
 	assert(timer_rate_in_sec > 0);
 	timer_rate_in_sec_ = timer_rate_in_sec;
 }
 
 
 Response RequestsHadler::handleNewRequest(string& url){
+	//handles the requests. if the number of requests reached the limit
+	// an error code 429 and a an error message will be return
 	if(current_requests_limit_ == 0){
 		ostringstream oss;
 		oss << "Rate limit exceeded. Try again in" << timer_rate_in_sec_ << "seconds";
@@ -36,6 +41,8 @@ Response RequestsHadler::handleNewRequest(string& url){
 }
 
 void RequestsHadler::updateRequestsLimit(boost::asio::deadline_timer* timer ){
+	//After the rate time passed,
+	//updating the current requests limit to be the requests limit
 	current_requests_limit_ = requests_limit_;
 	boost::posix_time::seconds interval(timer_rate_in_sec_);
 	timer->expires_at(timer->expires_at() + interval);
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,18 @@
 #include "lib/requests_handler/requests_handler.hpp"
 
 int main(int argc, char* argv[]) {
+	//creating pRequestsHandler object
+	// with requests_limit_ = 100 and timer_rate_in_sec_ = 60 seconds
+	auto pRequestsHandler = new RequestsHadler(100, 60);
 
-  auto pRequestsHandler = new RequestsHadler(100, 60);
-  boost::asio::io_service io_s;
-  boost::asio::deadline_timer timer(io_s);
-  boost::posix_time::seconds interval(pRequestsHandler->timer_rate_in_sec());
-  timer.expires_at(timer.expires_at() + interval);
-  timer.async_wait(std::bind(&RequestsHadler::updateRequestsLimit,pRequestsHandler, &timer));
-  io_s.run();
+	//initialising io_servuce and boost timer
+	boost::asio::io_service io_s;
+	boost::asio::deadline_timer timer(io_s);
+	boost::posix_time::seconds interval(pRequestsHandler->timer_rate_in_sec());
+	timer.expires_at(timer.expires_at() + interval);
+	timer.async_wait(std::bind(&RequestsHadler::updateRequestsLimit,pRequestsHandler, &timer));
+	io_s.run();
 
-  return 0;
+	  return 0;
 }
 
